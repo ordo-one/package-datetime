1caccd95250d611287a0970977af7dfc0610a9d5
Joakim Hassila <jocke@ordo.one>
2022-11-29 14:48:18 +0100

    Adding BenchmarkClock and nanoseconds convenience accessor for Duration.

--- Tests/DateTimeTests/DateTime.swift
+++ Tests/DateTimeTests/DateTime.swift
@@ -12,15 +12,30 @@ import XCTest
 final class DateTimeTests: XCTestCase {
     func testExample() throws {
         let time = InternalUTCClock().measure {
-            print("Time now: \(InternalUTCClock.now)")
-            print("Time now: \(InternalUTCClock.now._value.components)")
-            print("Resolution: \(InternalUTCClock().minimumResolution)")
+            print("InternalUTCClock Time now: \(InternalUTCClock.now)")
+            print("InternalUTCClock Time now: \(InternalUTCClock.now._value.components)")
+            print("InternalUTCClock Resolution: \(InternalUTCClock().minimumResolution)")
         }
-        print("Elapsed time in nanoseconds \(time.components.attoseconds / 1_000_000_000)")
+        print("InternalUTCClock Elapsed time in nanoseconds \(time.components.attoseconds / 1_000_000_000)")
         let time2 = InternalUTCClock().measure {
             _ = 47
         }
-        print("Elapsed time in nanoseconds for empty closure \(time2.components.attoseconds / 1_000_000_000)")
+        print("InternalUTCClock Elapsed time in nanoseconds for empty closure \(time2.components.attoseconds / 1_000_000_000)")
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+    }
+    func testBenchmarkClock() throws {
+        let time = BenchmarkClock().measure {
+            print("BenchmarkClock Time now: \(BenchmarkClock.now)")
+            print("BenchmarkClock Time now: \(BenchmarkClock.now._value.components)")
+            print("BenchmarkClock Resolution: \(BenchmarkClock().minimumResolution)")
+        }
+        print("BenchmarkClock Elapsed time in nanoseconds \(time.components.attoseconds / 1_000_000_000)")
+        let time2 = BenchmarkClock().measure {
+            _ = 47
+        }
+        print("BenchmarkClock Elapsed time in nanoseconds for empty closure \(time2.components.attoseconds / 1_000_000_000)")
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
