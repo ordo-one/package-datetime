46f14178a0927a678c971a3a3f2ac5c3e9e421e7
Joakim Hassila <jocke@ordo.one>
2023-02-10 13:46:19 +0100

    Fix padding for very small attoseconds values

--- Tests/DateTimeTests/DateTimeTests.swift
+++ Tests/DateTimeTests/DateTimeTests.swift
@@ -21,9 +21,9 @@ final class DateTimeTests: XCTestCase {
             _ = 47
         }
         print("""
-              InternalUTCClock Elapsed time in nanoseconds for empty closure \
-              \(time2.components.attoseconds / 1_000_000_000)
-              """)
+        InternalUTCClock Elapsed time in nanoseconds for empty closure \
+        \(time2.components.attoseconds / 1_000_000_000)
+        """)
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
@@ -40,11 +40,31 @@ final class DateTimeTests: XCTestCase {
             _ = 47
         }
         print("""
-              BenchmarkClock Elapsed time in nanoseconds for empty closure \
-              \(time2.components.attoseconds / 1_000_000_000)
-              """)
+        BenchmarkClock Elapsed time in nanoseconds for empty closure \
+        \(time2.components.attoseconds / 1_000_000_000)
+        """)
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
+    }
+
+    func testOutput() throws {
+        let now = InternalUTCClock.now
+        var description: String
+        var instant: InternalUTCClock.Instant
+        var fractionsOfSeconds: String.SubSequence
+
+        instant = InternalUTCClock.Instant(secondsComponent: now.seconds(), attosecondsComponent: 12_345)
+        description = "\(instant)"
+        fractionsOfSeconds = description.dropFirst("2022-03-10 14:15:56.".count)
+        XCTAssert(fractionsOfSeconds == "000000")
+
+        instant = InternalUTCClock.Instant(secondsComponent: now.seconds(),
+                                           attosecondsComponent: 123_456_000_000_000_000)
+        description = "\(instant)"
+        fractionsOfSeconds = description.dropFirst("2022-03-10 14:15:56.".count)
+        XCTAssert(fractionsOfSeconds == "123456")
+
+        print("\(instant)")
     }
 }
